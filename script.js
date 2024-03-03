c698f5a6cb956b8da40f9f88dd529a7f3f6cf747
mrinmay_khanra <102664340+mrinmaykhanra@users.noreply.github.com>
2024-03-03 17:32:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+    // You can add your form submission logic here
+    // For example, sending form data to a server using AJAX
+    // or displaying a success message
+    alert("Form submitted successfully!");
+});
